e3dc9443e7a57f399ab508eea45d223fde114b2e
Faizan <faizanqazi487@gmail.com>
2020-09-14 22:34:09 +0530

    lnitial commit

--- Python/Introduction/Arithmetic Operators.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    
-    # print(f"{a+b}\n{a-b}\n{a*b}")
-    # print("{0}\n{1}\n{2}".format((a+b), (a-b), (a*b)))
-    print(a+b, a-b, a*b, sep="\n")
