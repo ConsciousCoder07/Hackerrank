012d51413ae08f742aa325290b2123b34e2c4adb
Faizan <faizanqazi487@gmail.com>
2021-07-18 17:21:21 +0530

    Aggregation queries 16 & 17 (Completed)

--- SQL/03. Aggregation/015. Weather Observation Station 18.sql
+++ SQL/03. Aggregation/015. Weather Observation Station 18.sql
@@ -1,4 +1,6 @@
 -- https://www.hackerrank.com/challenges/weather-observation-station-18/problem
 
+-- Manhattan distance = |X1 – X2| + |Y1 – Y2|
+
 SELECT ROUND( ABS(MIN(LAT_N)-MAX(LAT_N)) + ABS(MIN(LONG_W)-MAX(LONG_W)) , 4 )
 FROM STATION;
